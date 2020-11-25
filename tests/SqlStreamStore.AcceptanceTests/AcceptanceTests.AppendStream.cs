b690e1e352ffe261a9d950ff50a1c43b3edb3b60
Rasmus Larsson <stoft@users.noreply.github.com>
2020-11-25 15:05:35 +0100

    wip

--- tests/SqlStreamStore.AcceptanceTests/AcceptanceTests.AppendStream.cs
+++ tests/SqlStreamStore.AcceptanceTests/AcceptanceTests.AppendStream.cs
@@ -665,7 +665,7 @@
             exception.ShouldBeOfType<WrongExpectedVersionException>(
                 ErrorMessages.AppendFailedWrongExpectedVersion(streamId, 10));
         }
-        
+
         [Theory, Trait("Category", "AppendStream")]
         [InlineData("stream/id")]
         [InlineData("stream%id")]
@@ -676,7 +676,7 @@
             result.CurrentVersion.ShouldBe(2);
             result.CurrentPosition.ShouldBeGreaterThanOrEqualTo(Fixture.MinPosition + 2L);
         }
-        
+
         [Theory, Trait("Category", "AppendStream")]
         [InlineData("stream/id")]
         [InlineData("stream%id")]
@@ -699,7 +699,7 @@
             result.CurrentVersion.ShouldBe(2);
             result.CurrentPosition.ShouldBeGreaterThanOrEqualTo(Fixture.MinPosition + 2L);
         }
-        
+
         [Theory, Trait("Category", "AppendStream")]
         [InlineData("stream/id")]
         [InlineData("stream%id")]
@@ -717,51 +717,50 @@
         {
             // Idempotency
             const string streamId = "stream-1";
-            
+
             var messages = CreateNewStreamMessages(1, 2);
             var tasks = new List<Task<AppendResult>>();
-            for(var index = 0; index < 10; index++)
+            for (var index = 0; index < 10; index++)
             {
                 tasks.Add(Store.AppendToStream(streamId, ExpectedVersion.NoStream, messages));
             }
-            
+
             var results = await Task.WhenAll(tasks);
 
             Assert.All(results, result => result.CurrentVersion.ShouldBe(1));
             Assert.All(results, result => result.CurrentPosition.ShouldBe(results[0].CurrentPosition));
         }
-        
+
         [Fact, Trait("Category", "AppendStream")]
         public async Task When_append_to_different_streams_concurrently_with_no_stream_expected_and_same_messages_then_should_have_expected_result()
         {
             // Idempotency
             const string streamPrefix = "stream-";
-            
+
             var messages = CreateNewStreamMessages(1, 2);
             var tasks = new List<Task<AppendResult>>();
-            for(var index = 0; index < 10; index++)
+            for (var index = 0; index < 10; index++)
             {
                 tasks.Add(Store.AppendToStream(streamPrefix + index, ExpectedVersion.NoStream, messages));
             }
-            
+
             var results = await Task.WhenAll(tasks);
 
             Assert.All(results, result => result.CurrentVersion.ShouldBe(1));
         }
 
-        [Fact, Trait("Category", "AppendStream")]
-        public async Task When_append_to_same_stream_concurrently_with_any_stream_expected_and_different_messages_then_should_not_throw()
+        [Fact, Trait("Category", "AppendStream2")]
+        public async Task When_append_to_same_stream_concurrently_with_expected_version_any_and_different_messages_then_should_not_throw()
         {
-            // Idempotency
             const string streamPrefix = "stream-";
-            
-            var messages = CreateNewStreamMessages(1, 2);
+
+            var messages = CreateNewStreamMessages(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
             var tasks = new List<Task<AppendResult>>();
-            for(var index = 0; index < 10; index++)
+            foreach (var message in messages)
             {
-                tasks.Add(Store.AppendToStream(streamPrefix + index, ExpectedVersion.NoStream, messages));
+                tasks.Add(Store.AppendToStream(streamPrefix + index, ExpectedVersion.Any, [message]));
             }
-            
+
             var results = await Task.WhenAll(tasks);
 
             Assert.All(results, result => result.CurrentVersion.ShouldBe(1));
